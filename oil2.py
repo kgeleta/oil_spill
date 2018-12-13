76fe0a98c45a88248724fd19cea956ee0af08258
Krzysztof Geleta <homar425@gmail.com>
2018-12-13 15:46:41 +0100

    refactored names

--- oil2.py
+++ oil2.py
@@ -42,71 +42,71 @@ class Cell:
 
 def initialize():
     # todo: refactor names
-    global test, next_test
-    test = [[Cell(0, False, [10, 0, 0, 0], [0, 0, 0, 0]) for j in range(n)] for i in range(n)]
-    next_test = [[Cell(0, False, [10, 0, 0, 0], [0, 0, 0, 0]) for j in range(n)] for i in range(n)]
+    global surface_layer, next_surface_layer
+    surface_layer = [[Cell(0, False, [10, 0, 0, 0], [0, 0, 0, 0]) for j in range(n)] for i in range(n)]
+    next_surface_layer = [[Cell(0, False, [10, 0, 0, 0], [0, 0, 0, 0]) for j in range(n)] for i in range(n)]
     for x in range(n):
         for y in range(n):
             if 35 < x < 40 and 35 < y < 40:
-                test[x][y].land = True
+                surface_layer[x][y].land = True
             if x < 45:
-                test[x][y].wind = [0, 0, 0, 0]
-                test[x][y].current = [0, 0, 1.5, 0]
+                surface_layer[x][y].wind = [0, 0, 0, 0]
+                surface_layer[x][y].current = [0, 0, 1.5, 0]
             if 45 < x < 55 and 25 < y < 35:
-                test[x][y].mass = 99
+                surface_layer[x][y].mass = 99
 
 
 def update():
-    global test, next_test
+    global surface_layer, next_surface_layer
     for i in range(n):
         for j in range(n):
             if i == 0 or j == 0 or i == n-1 or j == n-1:
-                next_test[i][j].mass = test[i][j].mass
-                next_test[i][j].land = test[i][j].land
-                next_test[i][j].wind = test[i][j].wind
-                next_test[i][j].current = test[i][j].current
+                next_surface_layer[i][j].mass = surface_layer[i][j].mass
+                next_surface_layer[i][j].land = surface_layer[i][j].land
+                next_surface_layer[i][j].wind = surface_layer[i][j].wind
+                next_surface_layer[i][j].current = surface_layer[i][j].current
             else:
-                if test[i][j].land:
+                if surface_layer[i][j].land:
                     # skpiuj wartosci z poprzedniej iteracji
-                    next_test[i][j].mass = test[i][j].mass
-                    next_test[i][j].land = test[i][j].land
-                    next_test[i][j].wind = test[i][j].wind
-                    next_test[i][j].current = test[i][j].current
-                if not test[i][j].land:
+                    next_surface_layer[i][j].mass = surface_layer[i][j].mass
+                    next_surface_layer[i][j].land = surface_layer[i][j].land
+                    next_surface_layer[i][j].wind = surface_layer[i][j].wind
+                    next_surface_layer[i][j].current = surface_layer[i][j].current
+                if not surface_layer[i][j].land:
                     # skopiuj wartosci z poprzedniej iteracji
-                    next_test[i][j].mass = test[i][j].mass
-                    next_test[i][j].land = test[i][j].land
-                    next_test[i][j].wind = test[i][j].wind
-                    next_test[i][j].current = test[i][j].current
+                    next_surface_layer[i][j].mass = surface_layer[i][j].mass
+                    next_surface_layer[i][j].land = surface_layer[i][j].land
+                    next_surface_layer[i][j].wind = surface_layer[i][j].wind
+                    next_surface_layer[i][j].current = surface_layer[i][j].current
 
                     # if neighbor is not a land:
                     # cardinal directions:
-                    if not test[i-1][j].land:# SOUTH
-                        w = ((test[i][j].current[SOUTH] + test[i-1][j].current[SOUTH])/2/current_max) +\
-                            R_w * ((test[i][j].wind[SOUTH] + test[i-1][j].wind[SOUTH])/2/wind_max)
-                        next_test[i][j].mass += m*((1+w)*test[i-1][j].mass - (1-w)*test[i][j].mass)
-                    if not test[i+1][j].land: # NORTH
-                        e = ((test[i][j].current[NORTH] + test[i+1][j].current[NORTH])/2/current_max) +\
-                            R_w * ((test[i][j].wind[NORTH] + test[i+1][j].wind[NORTH])/2/wind_max)
-                        next_test[i][j].mass += m*((1+e)*test[i+1][j].mass - (1-e)*test[i][j].mass)
-                    if not test[i][j-1].land: # EAST
-                        s = ((test[i][j].current[EAST] + test[i][j-1].current[EAST])/2/current_max) +\
-                            R_w * ((test[i][j].wind[EAST] + test[i][j-1].wind[EAST])/2/wind_max)
-                        next_test[i][j].mass += m*((1+s)*test[i][j-1].mass - (1-s)*test[i][j].mass)
-                    if not test[i][j+1].land:   # WEST
-                        north = ((test[i][j].current[WEST] + test[i][j+1].current[WEST])/2/current_max) +\
-                            R_w * ((test[i][j].wind[WEST] + test[i][j+1].wind[WEST])/2/wind_max)
-                        next_test[i][j].mass += m*((1+north)*test[i][j+1].mass - (1-north)*test[i][j].mass)
+                    if not surface_layer[i - 1][j].land:# SOUTH
+                        w = ((surface_layer[i][j].current[SOUTH] + surface_layer[i - 1][j].current[SOUTH]) / 2 / current_max) + \
+                            R_w * ((surface_layer[i][j].wind[SOUTH] + surface_layer[i - 1][j].wind[SOUTH]) / 2 / wind_max)
+                        next_surface_layer[i][j].mass += m * ((1 + w) * surface_layer[i - 1][j].mass - (1 - w) * surface_layer[i][j].mass)
+                    if not surface_layer[i + 1][j].land: # NORTH
+                        e = ((surface_layer[i][j].current[NORTH] + surface_layer[i + 1][j].current[NORTH]) / 2 / current_max) + \
+                            R_w * ((surface_layer[i][j].wind[NORTH] + surface_layer[i + 1][j].wind[NORTH]) / 2 / wind_max)
+                        next_surface_layer[i][j].mass += m * ((1 + e) * surface_layer[i + 1][j].mass - (1 - e) * surface_layer[i][j].mass)
+                    if not surface_layer[i][j - 1].land: # EAST
+                        s = ((surface_layer[i][j].current[EAST] + surface_layer[i][j - 1].current[EAST]) / 2 / current_max) + \
+                            R_w * ((surface_layer[i][j].wind[EAST] + surface_layer[i][j - 1].wind[EAST]) / 2 / wind_max)
+                        next_surface_layer[i][j].mass += m * ((1 + s) * surface_layer[i][j - 1].mass - (1 - s) * surface_layer[i][j].mass)
+                    if not surface_layer[i][j + 1].land:   # WEST
+                        north = ((surface_layer[i][j].current[WEST] + surface_layer[i][j + 1].current[WEST]) / 2 / current_max) + \
+                                R_w * ((surface_layer[i][j].wind[WEST] + surface_layer[i][j + 1].wind[WEST]) / 2 / wind_max)
+                        next_surface_layer[i][j].mass += m * ((1 + north) * surface_layer[i][j + 1].mass - (1 - north) * surface_layer[i][j].mass)
 
                     # intermediate directions:
-                    if not test[i-1][j-1].land:
-                        next_test[i][j].mass += m*d*(test[i-1][j-1].mass - test[i][j].mass)
-                    if not test[i-1][j+1].land:
-                        next_test[i][j].mass += m*d*(test[i-1][j+1].mass - test[i][j].mass)
-                    if not test[i+1][j-1].land:
-                        next_test[i][j].mass += m*d*(test[i+1][j-1].mass - test[i][j].mass)
-                    if not test[i+1][j+1].land:
-                        next_test[i][j].mass += m*d*(test[i+1][j+1].mass - test[i][j].mass)
+                    if not surface_layer[i - 1][j - 1].land:
+                        next_surface_layer[i][j].mass += m * d * (surface_layer[i - 1][j - 1].mass - surface_layer[i][j].mass)
+                    if not surface_layer[i - 1][j + 1].land:
+                        next_surface_layer[i][j].mass += m * d * (surface_layer[i - 1][j + 1].mass - surface_layer[i][j].mass)
+                    if not surface_layer[i + 1][j - 1].land:
+                        next_surface_layer[i][j].mass += m * d * (surface_layer[i + 1][j - 1].mass - surface_layer[i][j].mass)
+                    if not surface_layer[i + 1][j + 1].land:
+                        next_surface_layer[i][j].mass += m * d * (surface_layer[i + 1][j + 1].mass - surface_layer[i][j].mass)
 
     # druga petla - osadzanie na brzegu bierze wartosci z next_test wiec musi byc juz cala gotowa
     for i in range(n):
@@ -114,53 +114,53 @@ def update():
             if i == 0 or j == 0 or i == n-1 or j == n-1:
                 None
             else:
-                if not test[i][j].land:
+                if not surface_layer[i][j].land:
                     # jesli sasiad jest ladem i masa na tym ladzie mniejsza od max - czesc masy osadza sie na ladzie
                     # todo: m0 + P * m < C_max
-                    if next_test[i - 1][j].land and next_test[i - 1][j].mass <= C_max:
-                        next_test[i - 1][j].mass += P * next_test[i][j].mass
-                        next_test[i][j].mass -= P * next_test[i][j].mass
-                    if next_test[i + 1][j].land and next_test[i + 1][j].mass <= C_max:
-                        next_test[i + 1][j].mass += P * next_test[i][j].mass
-                        next_test[i][j].mass -= P * next_test[i][j].mass
-                    if next_test[i][j - 1].land and next_test[i][j - 1].mass <= C_max:
-                        next_test[i][j - 1].mass += P * next_test[i][j].mass
-                        next_test[i][j].mass -= P * next_test[i][j].mass
-                    if next_test[i][j + 1].land and next_test[i][j + 1].mass <= C_max:
-                        next_test[i][j + 1].mass += P * next_test[i][j].mass
-                        next_test[i][j].mass -= P * next_test[i][j].mass
+                    if next_surface_layer[i - 1][j].land and next_surface_layer[i - 1][j].mass <= C_max:
+                        next_surface_layer[i - 1][j].mass += P * next_surface_layer[i][j].mass
+                        next_surface_layer[i][j].mass -= P * next_surface_layer[i][j].mass
+                    if next_surface_layer[i + 1][j].land and next_surface_layer[i + 1][j].mass <= C_max:
+                        next_surface_layer[i + 1][j].mass += P * next_surface_layer[i][j].mass
+                        next_surface_layer[i][j].mass -= P * next_surface_layer[i][j].mass
+                    if next_surface_layer[i][j - 1].land and next_surface_layer[i][j - 1].mass <= C_max:
+                        next_surface_layer[i][j - 1].mass += P * next_surface_layer[i][j].mass
+                        next_surface_layer[i][j].mass -= P * next_surface_layer[i][j].mass
+                    if next_surface_layer[i][j + 1].land and next_surface_layer[i][j + 1].mass <= C_max:
+                        next_surface_layer[i][j + 1].mass += P * next_surface_layer[i][j].mass
+                        next_surface_layer[i][j].mass -= P * next_surface_layer[i][j].mass
 
-                    if next_test[i - 1][j - 1].land and next_test[i - 1][j - 1].mass <= C_max:
-                        next_test[i - 1][j - 1].mass += P * next_test[i][j].mass
-                        next_test[i][j].mass -= P * next_test[i][j].mass
-                    if next_test[i - 1][j + 1].land and next_test[i - 1][j + 1].mass <= C_max:
-                        next_test[i - 1][j + 1].mass += P * next_test[i][j].mass
-                        next_test[i][j].mass -= P * next_test[i][j].mass
-                    if next_test[i + 1][j - 1].land and next_test[i + 1][j - 1].mass <= C_max:
-                        next_test[i + 1][j - 1].mass += P * next_test[i][j].mass
-                        next_test[i][j].mass -= P * next_test[i][j].mass
-                    if next_test[i + 1][j + 1].land and next_test[i + 1][j + 1].mass <= C_max:
-                        next_test[i + 1][j + 1].mass += P * next_test[i][j].mass
-                        next_test[i][j].mass -= P * next_test[i][j].mass
-    test, next_test = next_test, test
+                    if next_surface_layer[i - 1][j - 1].land and next_surface_layer[i - 1][j - 1].mass <= C_max:
+                        next_surface_layer[i - 1][j - 1].mass += P * next_surface_layer[i][j].mass
+                        next_surface_layer[i][j].mass -= P * next_surface_layer[i][j].mass
+                    if next_surface_layer[i - 1][j + 1].land and next_surface_layer[i - 1][j + 1].mass <= C_max:
+                        next_surface_layer[i - 1][j + 1].mass += P * next_surface_layer[i][j].mass
+                        next_surface_layer[i][j].mass -= P * next_surface_layer[i][j].mass
+                    if next_surface_layer[i + 1][j - 1].land and next_surface_layer[i + 1][j - 1].mass <= C_max:
+                        next_surface_layer[i + 1][j - 1].mass += P * next_surface_layer[i][j].mass
+                        next_surface_layer[i][j].mass -= P * next_surface_layer[i][j].mass
+                    if next_surface_layer[i + 1][j + 1].land and next_surface_layer[i + 1][j + 1].mass <= C_max:
+                        next_surface_layer[i + 1][j + 1].mass += P * next_surface_layer[i][j].mass
+                        next_surface_layer[i][j].mass -= P * next_surface_layer[i][j].mass
+    surface_layer, next_surface_layer = next_surface_layer, surface_layer
 
 
 def to_matrix():
-    global test, next_test
+    global surface_layer, next_surface_layer
     mass_matrix = zeros([n, n])
     for i in range(n):
         for j in range(n):
-            if test[i][j].land:
+            if surface_layer[i][j].land:
                 mass_matrix[i, j] = (-200)
-            if not test[i][j].land:
-                mass_matrix[i, j] = test[i][j].mass
+            if not surface_layer[i][j].land:
+                mass_matrix[i, j] = surface_layer[i][j].mass
     if mass_matrix[10, 10] < 0:
         print("ALERT")
     return mass_matrix
 
 
 def observe():
-    global test, next_test
+    global surface_layer, next_surface_layer
     fig, ax = plt.subplots()
     mm = to_matrix()
     ax.imshow(mm, vmin=0, vmax=1, cmap=cmap, norm=norm)
@@ -169,7 +169,7 @@ def observe():
 
 initialize()
 
-for t in range(100):
+for t in range(90):
     if t % 10 == 0:
         observe()
     update()
